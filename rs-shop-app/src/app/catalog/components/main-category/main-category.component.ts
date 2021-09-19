9b1b531f296ea4c8dfc1ab4b1923809c5e0db681
GusOdd <gus@dynama.by>
2021-09-19 12:37:27 +0300

    feat: add main category page

--- rs-shop-app/src/app/catalog/components/main-category/main-category.component.ts
+++ rs-shop-app/src/app/catalog/components/main-category/main-category.component.ts
@@ -11,6 +11,7 @@ export class MainCategoryComponent {
   mainCategoryID?: string;
 
   constructor(private route: ActivatedRoute, public catalogService: CatalogService) {
-    this.route.params.subscribe((value) => (this.mainCategoryID = value.mainCategoryID));
+    this.route.params.subscribe((value) => this.mainCategoryID = value.mainCategoryID);
+    this.catalogService.getSubCategories(this.mainCategoryID!);
   }
 }
