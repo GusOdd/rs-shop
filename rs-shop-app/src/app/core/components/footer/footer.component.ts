b0d7f28c9d0a3e00cc499741cd8237f143a8f894
GusOdd <gus@dynama.by>
2021-09-19 13:58:42 +0300

    feat: add footer

--- rs-shop-app/src/app/core/components/footer/footer.component.ts
+++ rs-shop-app/src/app/core/components/footer/footer.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  faVk,
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faOdnoklassniki,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  vkIcon = faVk;
+  fbIcon = faFacebook;
+  instagramIcon = faInstagram;
+  youtubeIcon = faYoutube;
+  okIcon = faOdnoklassniki;
+}
